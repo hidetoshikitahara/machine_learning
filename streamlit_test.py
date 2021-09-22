95e21995736106cd1882c5f36c0c66cf351e0570
hidetoshikitahara <85331557+hidetoshikitahara@users.noreply.github.com>
2021-09-23 08:55:25 +0900

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -8,16 +8,17 @@ import streamlit as st
 
 # matplotlib.style.use('ggplt')
 
-start = '2021-06-01'
+start = '2021-09-01'
 end   = '2021-10-30'
 
 _nikkei = pdr.DataReader('^NKX','stooq',start,end)
 nikkei = pd.DataFrame(_nikkei.iloc[:,3])
 nikkei.columns = ['^NKX',]
+nikkei_change = nikkei.pct_change()
 
 st.title('日経平均')
-st.write(nikkei)
-st.line_chart(nikkei)
+st.write(nikkei_change)
+st.line_chart(nikkei_change)
 
 
 # stooq = StooqDailyReader(brand, start=start, end=end)
